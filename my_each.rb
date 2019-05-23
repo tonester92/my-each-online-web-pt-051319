111b1d1c524af3a9a414e707172d5db6dcd5cd6a
Antonio Blandin <antonio.blandin25@gmail.com>
2019-05-23 20:39:38 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,11 +1,8 @@
 def my_each(collection)
-  new_collection = []
-  i = 0 
-  while i < collection.length 
-    yield (collection[i])
+  i = 0
+  while i < collection.length
+    yield(collection[i])
     i += 1
-    new_collection << yield(collection[i])
-    return new_collection
   end
-end 
-
+  collection
+end
